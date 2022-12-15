335a6fecfd0d10668fd49d9c8adb62b075a78de6
pheni <yonathancardoso@outlook.fr>
2022-12-15 21:35:39 +0100

    avancement

--- views/articles/index.php
+++ views/articles/index.php
@@ -1,16 +1,20 @@
 <div class="content">
     <div class="container">
-        <div class="row row-cols-auto">
+        <div class="row row-cols-auto" style="justify-content: center;">
             <?php foreach($articles as $article): ?>
             <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="<?= $article['images'] ?>" alt="<?= $article['nom'] ?>">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $article['nom'] ?></h5>
-                        <p class="card-text"><?= $article['content'] ?></p>
-                        <a href="<?= BASE_DIR ?>/articles/details/<?= $article['slug'] ?>" class="btn btn-primary">En savoir plus</a>
+                <a href="<?= BASE_DIR ?>/articles/details/<?= $article['slug'] ?>" style="text-decoration: none; color: black">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="<?= $article['images'] ?>" alt="<?= $article['nom'] ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $article['nom'] ?></h5>
+                            <p class="card-text"><?= $article['content'] ?></p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><?= $article['prix'] ?> €</li>
+                        </ul>
                     </div>
-                </div>
+                </a>
             </div>
             <?php endforeach ?>
         </div>

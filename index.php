52d0c0a63f18d9bd53ffdddc07faa157ffc9ebce
pheni <yonathancardoso@outlook.fr>
2022-12-25 13:09:09 +0100

    essai de reparer le souci d'url / link

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php
 define('ROOT', str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
-define('BASE_DIR', dirname("index.php"));
+//define('BASE_DIR', dirname("index.php"));
+
+// Détermine le chemin absolu du fichier PHP actuellement exécuté
+$current_file = $_SERVER['SCRIPT_FILENAME'];
+
+// Détermine le chemin absolu du répertoire de la page actuelle
+$current_dir = dirname($current_file);
+
+// Remonte d'un niveau dans l'arborescence de répertoires jusqu'à la racine de votre projet
+$root_dir = dirname($current_dir);
+
+// Définit la constante BASE_DIR avec le chemin absolu de la racine de votre projet
+define('BASE_DIR', $root_dir);
+
+// Détermine l'URL de base de votre site
+$base_url = 'http://' . $_SERVER['HTTP_HOST'];
+
+// Détermine le chemin absolu du répertoire de la page actuelle
+$current_dir = __DIR__;
+
+// Remonte d'un niveau dans l'arborescence de répertoires jusqu'à la racine
+$root_dir = dirname($current_dir);
+
+// Calcule le chemin relatif de la racine de votre projet par rapport à la page actuelle
+$relative_path = substr($root_dir, strlen($current_dir));
+
+// Crée l'URL complète de la racine de votre projet en concaténant l'URL de base de votre site et le chemin relatif
+$root_url = $base_url . $relative_path;
+
+// Définit la constante BASE_URL avec l'URL complète de la racine de votre projet
+define('BASE_URL', $root_url);
 
 require_once(ROOT.'app/Model.php');
 require_once(ROOT.'app/Controller.php');
--- views/layout/default.php
+++ views/layout/default.php
@@ -17,7 +17,7 @@
     </title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="staticfiles/css/style.css">
+    <link rel="stylesheet" href="<?= BASE_DIR ?>/staticfiles/css/style.css">
 
     <script src="https://kit.fontawesome.com/1999efdd56.js" crossorigin="anonymous"></script>
 </head>
@@ -58,6 +58,7 @@
 </nav>
 
 <div class="hero-div">
+    <?= "LE CHEMIN : " . BASE_DIR ?>
     <?= $content ?>
 </div>
 

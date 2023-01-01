2bd43c4b882b155222edb2570c87106b35a15682
Phenixel <yonathancardoso@outlook.fr>
2023-01-01 12:09:08 +0100

    (fix) bdd

--- test.sql
+++ test.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db:3306
--- Généré le : jeu. 15 déc. 2022 à 20:36
--- Version du serveur : 5.7.37
--- Version de PHP : 8.0.15
+-- Généré le : dim. 01 jan. 2023 à 11:06
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,37 +28,38 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `article` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `slug` varchar(250) NOT NULL,
   `nom` varchar(255) DEFAULT NULL,
   `content` varchar(255) DEFAULT NULL,
+  `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `images` varchar(250) NOT NULL,
-  `prix` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `prix` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `article`
 --
 
-INSERT INTO `article` (`id`, `slug`, `nom`, `content`, `images`, `prix`) VALUES
-(1, 'galaxy_flip', 'Galaxy flip', 'Bonjour je suis un super téléphone', 'staticfiles/medias/articles/Galaxy_flip.webp', 1050),
-(2, 'galaxy_fold', 'Galaxy Fold', 'Bien mieux qu\'un iphone', 'staticfiles/medias/articles/Galaxy_fold.webp', 1545),
-(3, 'galaxy_s22', 'Galaxy S22', 'Hello je suis une desc', 'staticfiles/medias/articles/Galaxy_S22.webp', 834),
-(4, 'galaxy_s22_ultra', 'Galaxy S22 Ultra', 'Hello je suis une desc', 'staticfiles/medias/articles/Galaxy_S22_ultra.webp', 1005),
-(5, 'galaxy_s22_v2', 'Galaxy S22 v2', 'Hello je suis une desc', 'staticfiles/medias/articles/Galaxy_S22.webp', 834),
-(6, 'galaxy_book2_pro', 'Galaxy Book2 Pro 360', 'Le meilleur pc du monde', 'staticfiles/medias/articles/galaxy_book2.webp', 1500);
+INSERT INTO `article` (`id`, `slug`, `nom`, `content`, `description`, `images`, `prix`) VALUES
+(1, 'galaxy_flip', 'Galaxy flip', 'Bonjour je suis un super téléphone', 'Le Samsung Galaxy Flip est un smartphone pliable doté d\'un écran AMOLED de 6,7 pouces, d\'un processeur Qualcomm Snapdragon 865 et de 8 Go de RAM. Il possède un appareil photo principal de triple capteur de 12 mégapixels, ainsi qu\'un appareil photo avant de 10 mégapixels.', 'staticfiles/medias/articles/Galaxy_flip.webp', 1050),
+(2, 'galaxy_fold', 'Galaxy Fold', 'Bien mieux qu\'un iphone', 'Le Samsung Galaxy Fold est un smartphone pliable doté d\'un écran AMOLED Flexible de 7,3 pouces, d\'un processeur Qualcomm Snapdragon 855 et de 12 Go de RAM. Il possède un appareil photo principal de triple capteur de 12 mégapixels, ainsi qu\'un appareil photo avant de 10 mégapixels', 'staticfiles/medias/articles/Galaxy_fold.webp', 1545),
+(3, 'galaxy_s22', 'Galaxy S22', 'Hello je suis une desc', 'Le Samsung Galaxy S22 est un smartphone haut de gamme doté d\'un écran AMOLED de 6,2 pouces, d\'un processeur Qualcomm Snapdragon 888 et de 8 Go de RAM. Il possède un appareil photo principal de 64 mégapixels et un appareil photo avant de 32 mégapixels', 'staticfiles/medias/articles/Galaxy_S22.webp', 834),
+(4, 'galaxy_s22_ultra', 'Galaxy S22 Ultra', 'Hello je suis une desc', 'Le Samsung Galaxy S22 Ultra est un smartphone haut de gamme doté d\'un écran AMOLED de 6,9 pouces avec une résolution de 1440 x 3200 pixels. Il est alimenté par un processeur Qualcomm Snapdragon 888 et dispose de 12 Go de RAM et de 256 Go de stockage interne (extensible via microSD). Il possède également un appareil photo principal de 108 mégapixels, ainsi qu\'un appareil photo avant de 40 mégapixels.', 'staticfiles/medias/articles/Galaxy_S22_ultra.webp', 1005),
+(5, 'galaxy_s22_v2', 'Galaxy S22 v2', 'Hello je suis une desc', 'Le Samsung Galaxy S22 est un smartphone de milieu de gamme doté d\'un écran Super AMOLED de 6,5 pouces avec une résolution de 1080 x 2400 pixels. Il est alimenté par un processeur Samsung Exynos 9609 et dispose de 6 Go de RAM et de 128 Go de stockage interne (extensible via microSD). Il possède également un appareil photo principal de 64 mégapixels, ainsi qu\'un appareil photo avant de 32 mégapixels.', 'staticfiles/medias/articles/Galaxy_S22.webp', 834),
+(6, 'galaxy_book2_pro', 'Galaxy Book2 Pro 360', 'Le meilleur pc du monde', 'Le Samsung Galaxy Book2 est un ordinateur portable 2-en-1 doté d\'un écran Super AMOLED de 12 pouces avec une résolution de 2160 x 1440 pixels. Il est alimenté par un processeur Qualcomm Snapdragon 850 et dispose de 4 Go de RAM et de 128 Go de stockage interne (extensible via microSD). Il possède également une caméra frontale de 5 mégapixels et une batterie qui peut durer jusqu\'à 20 heures.', 'staticfiles/medias/articles/galaxy_book2.webp', 1500);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `panier`
+-- Structure de la table `pannier`
 --
 
-CREATE TABLE `panier` (
-  `id` int(11) NOT NULL,
-  `id_article` int(11) NOT NULL,
-  `quantite` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `pannier` (
+  `id` int NOT NULL,
+  `id_article` int NOT NULL,
+  `quantite` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -67,11 +68,11 @@ CREATE TABLE `panier` (
 --
 
 CREATE TABLE `Users` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(250) DEFAULT NULL,
   `email` varchar(250) DEFAULT NULL,
   `password` varchar(250) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `Users`
@@ -79,7 +80,8 @@ CREATE TABLE `Users` (
 
 INSERT INTO `Users` (`id`, `name`, `email`, `password`) VALUES
 (1, 'phen', 'phen@phen.fr', 'Azerty123'),
-(2, 'aaron', 'astico@wanadoo.fr', 'Azerty123');
+(2, 'aaron', 'astico@wanadoo.fr', 'Azerty123'),
+(3, 'demo', 'demo@demo.fr', 'demo');
 
 --
 -- Index pour les tables déchargées
@@ -112,19 +114,19 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT pour la table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `pannier`
 --
 ALTER TABLE `pannier`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
